6dd5ed70307c2869d42095748f59dd7a57f55fcf
Morten Sorvig <msorvig@gmail.com>
2010-10-10 09:53:54 +0200

    Split out units info

--- axis.js
+++ axis.js
@@ -4,21 +4,6 @@
 // Copyright 2010 Morten Johan Sørvig
 // Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 
-// Unit stats.
-// Array of: [Unit Name, Attack Rating, Defence Rating, IPC Cost, Flying,
-//               Participates in Land Battles, Participates Sea Battles]
-
-
-var UnitStats = [
-    ["Infantry",            1, 2, 3, false, true, false],
-    ["Mechanized Infantry", 1, 2, 4, false, true, false],
-    ["Artillery",           2, 2, 4, false, true, false],
-    ["Tank",                3, 3, 6, false, true, false],
-    ["Fighter",             3, 4, 10, true, true, false],
-    ["Tactical Bomber",     3, 3, 11, true, true, false],
-    ["Bomber",              4, 1, 11, true, true, false],
-];
-
 // Unit selector
 // Input:
 //      selectorDivElement - the div element that should contain the unit selector ui
---
+++ units.js
@@ -0,0 +1,13 @@
+// Unit stats.
+// Array of: [Unit Name, Attack Rating, Defence Rating, IPC Cost, Flying,
+//               Participates in Land Battles, Participates Sea Battles]
+
+var UnitStats = [
+    ["Infantry",            1, 2, 3, false, true, false],
+    ["Mechanized Infantry", 1, 2, 4, false, true, false],
+    ["Artillery",           2, 2, 4, false, true, false],
+    ["Tank",                3, 3, 6, false, true, false],
+    ["Fighter",             3, 4, 10, true, true, false],
+    ["Tactical Bomber",     3, 3, 11, true, true, false],
+    ["Bomber",              4, 1, 11, true, true, false],
+];
